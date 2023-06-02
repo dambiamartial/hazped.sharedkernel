c3d930d7601fb9dfb4486c0540bbf0f2a8eb8672
Martial DAMBIA <dambiamartial@gmail.com>
2023-06-02 09:53:05 +0100

    Adding Twilio text message sender

---
+++ Common/OTPGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace hazped.sharedkernel.Common;
+
+public static class OTPGenerator
+{
+    public static string GenerateOTP(int otpdigitNumber)
+    {
+        string num = "0123456789";
+        int len = num.Length - 1;
+        string otp = string.Empty;
+        string finalDigit;
+        int getIndex;
+        for (int i = 0; i < otpdigitNumber; i++)
+        {
+            do
+            {
+                getIndex = new Random().Next(0, len);
+                finalDigit = num.ToCharArray()[getIndex].ToString();
+            } while (otp.IndexOf(finalDigit) != -1);
+            otp += finalDigit;
+        }
+        return otp;
+    }
+}
---
+++ Common/TwilioTextMessageSender.cs
@@ -0,0 +1,34 @@
+﻿using Twilio;
+using Twilio.Exceptions;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace hazped.sharedkernel.Common;
+
+public static class TwilioTextMessageSender
+{
+    public static async Task<bool> SendMessage(string to, string body, string sid, string authToken, string number)
+    {
+        TwilioClient.Init(sid, authToken);
+
+        bool isSucceed = false;
+
+        try
+        {
+            var message = await MessageResource.CreateAsync(
+            body: body,
+            from: new Twilio.Types.PhoneNumber(number),
+            to: new Twilio.Types.PhoneNumber(to)
+            );
+            isSucceed = true;
+            Console.WriteLine(message.Sid);
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine($"Twilio Error {e.Code} - {e.MoreInfo}");
+        }
+        finally { }
+
+        return isSucceed;
+    }
+}
