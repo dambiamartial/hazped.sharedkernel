04b1fe524e9a3532e1623eb5bb5d83a887a05a93
Martial DAMBIA <dambiamartial@gmail.com>
2023-08-12 09:13:14 +0100

    update

--- Interfaces/IGenericRepository.cs
+++ Interfaces/IGenericRepository.cs
@@ -10,4 +10,5 @@ public interface IGenericRepository<T> where T : class
     void Delete(T entity);
     void DeleteRange(IEnumerable<T> entities);
     void Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
 }
